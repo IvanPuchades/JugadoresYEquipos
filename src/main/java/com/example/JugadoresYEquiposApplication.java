89a6d00434c5d43002b3e78fc20131d87bfc320b
dam <puchadesivan@gmail.com>
2016-10-21 18:53:42 +0200

    Initial commit

---
+++ src/main/java/com/example/JugadoresYEquiposApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugadoresYEquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugadoresYEquiposApplication.class, args);
+	}
+}
