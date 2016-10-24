c8ad0de48f0da0d1a0cf36d9df3a67ef4711a095
usu32 <usu32>
2016-10-24 15:58:45 +0200

    Introduciendo equipos y jugadores

---
+++ src/main/java/com/example/repository/EquipoRepository.java
@@ -0,0 +1,8 @@
+package com.example.repository;
+
+/**
+ * Created by usu32 on 24/10/2016.
+ */
+public interface EquipoRepository
+{
+}
---
+++ src/main/java/com/example/repository/JugadorRepository.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+
+/**
+ * Created by usu32 on 24/10/2016.
+ */
+public interface JugadorRepository {
+}
