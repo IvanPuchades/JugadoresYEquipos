d72ff28964b5f60839c14dedf72fe0eb2370d225
dam <puchadesivan@gmail.com>
2016-11-07 16:10:59 +0100

    Introduciendo los ultimos cambios

--- src/main/java/com/example/JugadoresYEquiposApplication.java
+++ src/main/java/com/example/JugadoresYEquiposApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.services.equipoServices;
 import com.example.services.jugadorServices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,5 +12,6 @@ public class JugadoresYEquiposApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(JugadoresYEquiposApplication.class, args);
 		context.getBean(jugadorServices.class).testJugador();
+		context.getBean(equipoServices.class).testEquipos();
 	}
 }
